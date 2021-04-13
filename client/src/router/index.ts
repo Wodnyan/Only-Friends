cac8a50a4bffb958ff4f2c503ce0c77a2c111c15
Albert Gergo <gergoalbert.gta@gmail.com>
2021-04-13 22:02:46 +0300

    feat: login page

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -7,15 +7,16 @@ const routes: Array<RouteRecordRaw> = [
     name: "Home",
     component: Home,
   },
-  // {
-  //   path: "/about",
-  //   name: "About",
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () =>
-  //     import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  // },
+  {
+    path: "/auth/sign-up",
+    name: "SignUp",
+    component: () => import("../views/auth/SignUp.vue"),
+  },
+  {
+    path: "/auth/login",
+    name: "Login",
+    component: () => import("../views/auth/Login.vue"),
+  },
 ];
 
 const router = createRouter({
