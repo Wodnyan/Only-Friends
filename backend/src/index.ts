421579cad0d650583d04c0055ec5fd4e077318bc
Albert Gergo <gergoalbert.gta@gmail.com>
2021-04-14 20:54:32 +0300

    feat: single source of truth for db connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,17 @@
-import { MikroORM } from "@mikro-orm/core";
+import "reflect-metadata";
 import { PORT, __prod__ } from "./constants";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
 import { UserResolver } from "./resolvers/user";
-import microConfig from "./mikro-orm.config";
+import { connection } from "./db";
 import dotenv from "dotenv";
 import express from "express";
-import "reflect-metadata";
 
 dotenv.config();
 
 (async () => {
-  const orm = await MikroORM.init(microConfig);
+  const orm = await connection();
   await orm.getMigrator().up();
 
   const apolloServer = new ApolloServer({
