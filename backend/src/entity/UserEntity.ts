8232e754106a1168e3ad912b2e9fee2aaeda04c2
Albert Gergo <gergoalbert.gta@gmail.com>
2021-05-28 22:30:30 +0300

    mend
    feat: apollo server, cors, session setup

--- backend/src/entity/UserEntity.ts
+++ backend/src/entity/UserEntity.ts
@@ -1,3 +1,4 @@
+import { Field, ObjectType } from "type-graphql";
 import {
   Column,
   CreateDateColumn,
@@ -7,28 +8,40 @@ import {
 } from "typeorm";
 
 @Entity()
+@ObjectType()
 export class User {
+  @Field()
   @PrimaryGeneratedColumn("uuid")
   id: string;
 
+  @Field()
   @Column({ unique: true })
   username: string;
 
+  @Field()
+  @Column()
+  fullName: string;
+
+  @Field()
   @Column({ unique: true })
   email: string;
 
   @Column()
   password: string;
 
+  @Field()
   @Column({ nullable: true })
   avatar?: string;
 
+  @Field()
   @Column({ default: true })
   isActive: boolean;
 
+  @Field()
   @CreateDateColumn()
   createdAt: Date;
 
+  @Field()
   @UpdateDateColumn()
   updatedAt: Date;
 }
